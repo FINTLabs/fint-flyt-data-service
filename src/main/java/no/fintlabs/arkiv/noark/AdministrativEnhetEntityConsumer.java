7f738cbc98c23937784869e6457f4865445ad559
Trond Magnus Sevre <trond@fintlabs.no>
2021-11-15 15:14:43 +0100

    FST-67 #comment Remove unused code

--- src/main/java/no/fintlabs/arkiv/noark/AdministrativEnhetEntityConsumer.java
+++ src/main/java/no/fintlabs/arkiv/noark/AdministrativEnhetEntityConsumer.java
@@ -1,7 +1,6 @@
 package no.fintlabs.arkiv.noark;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.JsonDeserializer;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.extern.slf4j.Slf4j;
 import no.fint.model.resource.arkiv.noark.AdministrativEnhetResource;
--- src/main/java/no/fintlabs/arkiv/noark/KlassifikasjonssystemConsumer.java
+++ src/main/java/no/fintlabs/arkiv/noark/KlassifikasjonssystemConsumer.java
@@ -1,17 +1,21 @@
-//package no.fintlabs.arkiv.noark;
-//
-//import lombok.extern.slf4j.Slf4j;
-//import no.fint.model.resource.arkiv.kodeverk.KlassifikasjonstypeResource;
-//import no.fintlabs.kafka.EntityMessage;
-//import org.springframework.kafka.annotation.KafkaListener;
-//import org.springframework.stereotype.Component;
-//
-//@Slf4j
-//@Component
-//public class KlassifikasjonssystemConsumer {
-//
-//    @KafkaListener(topics = "arkiv.noark.klassifikasjonssystem")
-//    public void processMessage(KlassifikasjonstypeResource entityMessage) {
-//        log.info("Message received by consumer 1: " + entityMessage.toString());
-//    }
-//}
+package no.fintlabs.arkiv.noark;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.extern.slf4j.Slf4j;
+import no.fint.model.resource.arkiv.noark.KlassifikasjonssystemResource;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Component;
+
+@Slf4j
+@Component
+public class KlassifikasjonssystemConsumer {
+    private final ObjectMapper mapper = new ObjectMapper();
+
+    @KafkaListener(topics = "entity.arkiv.noark.klassifikasjonssystem")
+    public void processMessage(ConsumerRecord<String, String> row) throws JsonProcessingException {
+        KlassifikasjonssystemResource value = mapper.readValue(row.value(), KlassifikasjonssystemResource.class);
+        log.info("Message received by consumer 1: " + value.toString());
+    }
+}
