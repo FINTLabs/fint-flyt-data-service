600df193d7825683d83836aa01666948f7355b42
Trond Magnus Sevre <trond@fintlabs.no>
2021-11-15 15:03:26 +0100

    FST-67 #comment Change from byte[] to Object

--- src/main/java/no/fintlabs/arkiv/noark/AdministrativEnhetEntityConsumer.java
+++ src/main/java/no/fintlabs/arkiv/noark/AdministrativEnhetEntityConsumer.java
@@ -1,8 +1,11 @@
 package no.fintlabs.arkiv.noark;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonDeserializer;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.extern.slf4j.Slf4j;
 import no.fint.model.resource.arkiv.noark.AdministrativEnhetResource;
-import no.fintlabs.kafka.EntityMessage;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Component;
 
@@ -10,8 +13,11 @@ import org.springframework.stereotype.Component;
 @Component
 public class AdministrativEnhetEntityConsumer {
 
-    @KafkaListener(topics = "arkiv.noark.administrativenhet")
-    public void processMessage(EntityMessage<AdministrativEnhetResource> administrativEnhetResource) {
-        log.info("Message received by consumer 1: " + administrativEnhetResource.toString());
+    private final ObjectMapper mapper = new ObjectMapper();
+
+    @KafkaListener(topics = "entity.arkiv.noark.administrativenhet")
+    public void processMessage(ConsumerRecord<String, String> row) throws JsonProcessingException {
+        AdministrativEnhetResource value = mapper.readValue(row.value(), AdministrativEnhetResource.class);
+        log.info("Message received by consumer 1: " + value.toString());
     }
 }
--- src/main/java/no/fintlabs/arkiv/noark/KlassifikasjonssystemConsumer.java
+++ src/main/java/no/fintlabs/arkiv/noark/KlassifikasjonssystemConsumer.java
@@ -1,17 +1,17 @@
-package no.fintlabs.arkiv.noark;
-
-import lombok.extern.slf4j.Slf4j;
-import no.fint.model.resource.arkiv.kodeverk.KlassifikasjonstypeResource;
-import no.fintlabs.kafka.EntityMessage;
-import org.springframework.kafka.annotation.KafkaListener;
-import org.springframework.stereotype.Component;
-
-@Slf4j
-@Component
-public class KlassifikasjonssystemConsumer {
-
-    @KafkaListener(topics = "arkiv.noark.klassifikasjonssystem")
-    public void processMessage(KlassifikasjonstypeResource entityMessage) {
-        log.info("Message received by consumer 1: " + entityMessage.toString());
-    }
-}
+//package no.fintlabs.arkiv.noark;
+//
+//import lombok.extern.slf4j.Slf4j;
+//import no.fint.model.resource.arkiv.kodeverk.KlassifikasjonstypeResource;
+//import no.fintlabs.kafka.EntityMessage;
+//import org.springframework.kafka.annotation.KafkaListener;
+//import org.springframework.stereotype.Component;
+//
+//@Slf4j
+//@Component
+//public class KlassifikasjonssystemConsumer {
+//
+//    @KafkaListener(topics = "arkiv.noark.klassifikasjonssystem")
+//    public void processMessage(KlassifikasjonstypeResource entityMessage) {
+//        log.info("Message received by consumer 1: " + entityMessage.toString());
+//    }
+//}
