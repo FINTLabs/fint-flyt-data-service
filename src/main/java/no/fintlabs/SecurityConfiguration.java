7281c3c30e4fa35114d3dc0c0a2e22b3e2e82e40
Frode Sjovatsen <fsjovatsen@users.noreply.github.com>
2022-04-03 22:39:12 +0200

    Fst 149 data service needs security (#3)
    * FST-149 #comment Upgraded spring boot + added security deps and config #in-progress
    
    * FST-149 #comment Increased http header size #in-progress

---
+++ src/main/java/no/fintlabs/SecurityConfiguration.java
@@ -0,0 +1,31 @@
+package no.fintlabs;
+
+import no.vigoiks.resourceserver.security.FintJwtUserConverter;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
+import org.springframework.security.config.web.server.SecurityWebFiltersOrder;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.web.server.SecurityWebFilterChain;
+
+@EnableWebFluxSecurity
+public class SecurityConfiguration {
+
+    @Value("${fint.integration.service.authorized-org-id:vigo.no}")
+    private String authorizedOrgId;
+
+    @Bean
+    SecurityWebFilterChain springSecurityFilterChain(ServerHttpSecurity http) {
+        http
+                .authorizeExchange((authorize) -> authorize
+                        .pathMatchers("/**")
+                        .hasAnyAuthority("ORGID_" + authorizedOrgId)
+                        .anyExchange()
+                        .authenticated())
+                .addFilterBefore(new AuthorizationLogFilter(), SecurityWebFiltersOrder.AUTHENTICATION)
+                .oauth2ResourceServer((resourceServer) -> resourceServer
+                        .jwt()
+                        .jwtAuthenticationConverter(new FintJwtUserConverter()));
+        return http.build();
+    }
+}
--- src/main/java/no/fintlabs/cache/FintCacheEventListener.java
+++ src/main/java/no/fintlabs/cache/FintCacheEventListener.java
@@ -1,5 +1,7 @@
 package no.fintlabs.cache;
 
+import org.ehcache.event.CacheEventListener;
+
 public interface FintCacheEventListener<K, V> {
 
     void onEvent(FintCacheEvent<K, V> event);
